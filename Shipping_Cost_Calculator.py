c18263c69abbe15720c02d9d50d3c27d05d76769
Manal4888 <manalateyyat4@gmail.com>
2025-12-19 17:30:39 +0300

    update .py file using vim

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,4 +1,6 @@
-# Shipping Cost Calculator
+
+
+
 
 ## Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))
@@ -9,4 +11,5 @@ shipping_cost = weight * rate
 
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
+# Here is a new update by <Manal4888>
 
